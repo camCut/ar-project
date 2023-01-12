08da52c621fcc56620673a24222326fadb581bf3
gebauer <gebauer@werdenktwas.de>
2023-01-12 10:38:12 +0100

    refactor: 💡 add router/query

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,15 @@
 <template>
  <div >
- <ArTest5 v-if="true"/>
+ <router-view></router-view>
  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import ArTest from "./components/ArTest.vue";
-import ArTest2 from "./components/ArTest2.vue";
-import ArTest3 from "./components/ArTest3.vue";
-import ArTest4 from "./components/ArTest4.vue";
-import ArTest5 from "./components/ArTest5.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    ArTest5,
   },
 });
 </script>
@@ -31,5 +25,6 @@ export default defineComponent({
 }
 video {
   display: block !important;
+  aspect-ratio: 1/1;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,23 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import {createApp} from 'vue';
+import App from './App.vue';
+import ArComponent from "./components/ArComponent.vue";
 
-const app = createApp(App)
-app.mount("#app");
+import {createRouter, createWebHashHistory} from 'vue-router';
+
+const routes = [
+  {
+    path: '/',
+    component: ArComponent,
+    props: (route: any) => ({orientation: Number(route.query.orientation)}),
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  // history: createWebHistory(),
+  routes,
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
