61b9ba26e306d9bf92051d16af5db67749b4ac81
gebauer <gebauer@werdenktwas.de>
2023-01-24 10:13:31 +0100

    refactor: 💡 new pattern

--- src/components/ArComponentImageAframe.vue
+++ src/components/ArComponentImageAframe.vue
@@ -16,7 +16,7 @@
     </div> -->
 
     <img
-      src="../assets/img/pattern-wdw-pattern.png"
+      src="../assets/img/pattern-pattern-wdw-pattern_2.png"
       v-show="trackerOpened"
       style="position: relative"
     />
@@ -39,7 +39,7 @@
         position="1 20 1"
       ></a-light>
       <a-entity light="type: ambient; color: #CCC"></a-entity>
-      <a-marker type="pattern" url="../../data/pattern-wdw-pattern.patt">
+      <a-marker type="pattern" url="../../data/pattern-pattern-wdw-pattern_2.patt">
         <a-plane
           color="#ffff00"
           height="1"
@@ -144,16 +144,8 @@ export default {
     }
 
     onMounted(() => {
-      const el = document.querySelector("a-scene");
-      el.addEventListener("markerFound", () => {
-        console.log('client', document.querySelector('video').clientHeight);
-        console.log('client', document.querySelector('video').clientWidth);
-        console.log("marker found");
-        const videoStyle = document.querySelector('video')
-         document.querySelector("a-scene").setAttribute('style', `width: ${videoStyle.clientWidth};height: ${videoStyle.clientHeight}`)
-        
-        // // addBox([2.5, 2.5, 2.5], [0, 0, 0], [0, 0, 0], "#4CC3D9");
-        addModel(
+
+       addModel(
           "https://cdn.aframe.io/examples/ar/models/triceratops/scene.gltf",
           [0.01, 0.01, 0.01],
           [0, 0, 0],
@@ -161,7 +153,24 @@ export default {
         );
 
         addText("Triceratops", [1, 1, 1], [0, 1.5, 0], [0, 0, 0]);
-      });
+      const el = document.querySelector("a-scene");
+      // el.addEventListener("markerFound", () => {
+      //   console.log('client', document.querySelector('video').clientHeight);
+      //   console.log('client', document.querySelector('video').clientWidth);
+      //   console.log("marker found");
+      //   const videoStyle = document.querySelector('video')
+      //    document.querySelector("a-scene").setAttribute('style', `width: ${videoStyle.clientWidth};height: ${videoStyle.clientHeight}`)
+        
+      //   // // addBox([2.5, 2.5, 2.5], [0, 0, 0], [0, 0, 0], "#4CC3D9");
+      //   addModel(
+      //     "https://cdn.aframe.io/examples/ar/models/triceratops/scene.gltf",
+      //     [0.01, 0.01, 0.01],
+      //     [0, 0, 0],
+      //     [0, 0, 0]
+      //   );
+
+      //   addText("Triceratops", [1, 1, 1], [0, 1.5, 0], [0, 0, 0]);
+      // });
     });
 
     function changePlaneColor() {
